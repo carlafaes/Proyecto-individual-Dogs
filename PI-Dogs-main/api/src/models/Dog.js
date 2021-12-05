2dac9cd96f93f127e269ce640ac8e427dfc2ba53
carlafaes <carlafaes18@hotmail.com>
2021-12-05 17:29:45 -0300

    1

--- PI-Dogs-main/api/src/models/Dog.js
+++ PI-Dogs-main/api/src/models/Dog.js
@@ -8,5 +8,30 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    weight:{
+      type:DataTypes.STRING,
+    },
+    height:{
+      type:DataTypes.STRING,
+    },
+    bred_for:{
+      type:DataTypes.STRING,
+    },
+    breed_group:{
+      type:DataTypes.STRING,
+
+    },
+    life_span:{
+      type:DataTypes.STRING,
+    },
+    temperament:{
+      type:DataTypes.STRING,
+    },
+    origin:{
+      type:DataTypes.STRING,
+    },
+    image:{
+      type:DataTypes.STRING,
+    },
   });
 };
