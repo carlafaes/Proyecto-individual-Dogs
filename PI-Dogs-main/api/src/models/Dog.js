407f1f60a763665706a5a45740d359b801d70d2d
carlafaes <carlafaes18@hotmail.com>
2021-12-05 21:07:24 -0300

    2

--- PI-Dogs-main/api/src/models/Dog.js
+++ PI-Dogs-main/api/src/models/Dog.js
@@ -4,6 +4,12 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue:DataTypes.UUIDV1,
+      allowNull:false,
+      primaryKey:true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
