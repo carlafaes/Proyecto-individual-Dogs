6b708554e1395e09ebff3412cd348a07beb0674e
carlafaes <carlafaes18@hotmail.com>
2021-12-10 19:09:01 -0300

    routes

--- PI-Dogs-main/api/src/routes/dogRoutes.js
+++ PI-Dogs-main/api/src/routes/dogRoutes.js
@@ -10,14 +10,7 @@ router.get('/dogs', async (req, res)=>{
 //     const name= req.query.name;// recibo el nombre por query.
 //  //   let totalDogs= await getAllInfo();//espero a la funcion controladora
        let getApi2= await reqApi()
-//     if (name){
-//         //let nameDogs= await getApi2.filter(e => e.name.toLowerCase().includes(name.toLowerCase()));
-//         ///nameDogs.length? res.status(200).send(nameDogs): res.status(404).send('Name dog not found');
-//     }
-//     else{
-//         res.status(200).send(getApi2);
-//     }
-  res.status(200).send(getApi2);
+    res.status(200).send(getApi2);
 } )
 
 
